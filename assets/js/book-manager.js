7886361ef1036b3e2beaf2c563226357660a7d35
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-08 17:33:41 +0200

    class based work

--- assets/js/book-manager.js
+++ assets/js/book-manager.js
@@ -3,7 +3,7 @@ const inputAuthor = document.querySelector('.input-author');
 const addButton = document.querySelector('.add-button');
 const bookShelf = document.querySelector('.book-shelf');
 let bookCard = document.createElement('div');
-
+let Mybooks = [];
 class BookList {
   constructor(title, author) {
     this.title = title;
@@ -18,40 +18,57 @@ class Book {
     }
   }
 
-  static saveToLocalStorage() {}
+  static getToloStorage() {
+    let book;
+    if (!localStorage.getItem('books')) {
+      book = [];
+    } else {
+      book = JSON.parse(localStorage.getItem('books'));
+    }
+    return book;
+  }
+
+  static addToLocalStorage(book) {
+    let books = Book.getToloStorage();
+    console.log(book, 'add');
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeToLocalStorage(id) {
+    let books = Book.getToloStorage();
+    books.forEach((book, index) => {
+      console.log('Book removed', index);
+      if (index === id) {
+        book.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 
   static clearFields() {
     inputTitle.value = '';
     inputAuthor.value = '';
   }
+
   static addBook(book) {
     bookCard.innerHTML += `
-   <div>
-   <p>${book.title}</p>
-   <p>${book.author}</p>
-   <button type="submit" class="remove-button">Remove</button>
-   </div
-   `;
+        <div>
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button type="submit" class="remove-button">Remove</button>
+        </div
+        `;
     bookShelf.appendChild(bookCard);
   }
 
   static showBook() {
-    const obj = [
-      {
-        title: 'geo',
-        author: 'RDC',
-      },
-      {
-        title: 'histore',
-        author: 'Rwanda',
-      },
-      {
-        title: 'histore 3',
-        author: 'Rwanda',
-      },
-    ];
+    const obj = Book.getToloStorage();
 
-    obj.forEach((book) => {
+    obj.forEach((book, index) => {
+      console.log('itemsss', index);
+      Mybooks.push(index);
       Book.addBook(book);
     });
   }
@@ -65,11 +82,13 @@ document.querySelector('.book-input').addEventListener('submit', (e) => {
   if (title && author) {
     const bookList = new BookList(title, author);
     Book.addBook(bookList);
+    Book.addToLocalStorage(bookList);
     Book.clearFields();
   }
 });
 
 bookShelf.addEventListener('click', function (event) {
+  //   console.log('id', +event.target.dataset.id);
   Book.removeButton(event.target);
 });
 document.addEventListener('DOMContentLoaded', Book.showBook);
