c085ca94f62898d03338f7188f40d67a768dc588
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-08 07:55:00 +0200

    Add shoBook function to display books on the page

--- assets/js/book-manager.js
+++ assets/js/book-manager.js
@@ -7,12 +7,26 @@ let bookList = [];
 
 // add function
 
-function addBook(){
-    const title = inputTitle.value;
-    const author = inputAuthor.value;
-    const book = { title , author };
-    if(title && author){
-        bookList.push(book)
-    }
+function addBook() {
+  const title = inputTitle.value;
+  const author = inputAuthor.value;
+  const book = { title, author };
+  if (title && author) {
+    bookList.push(book);
+    showBook(book);
+    console.log(book);
+  }
 }
 
+function showBook(book) {
+  const bookCard = document.createElement('div');
+
+  bookCard.innerHTML = `
+ <h3>${book.title}</h3>
+ <h3>${book.author}</h3>
+ `;
+  bookShelf.appendChild(bookCard);
+  return book;
+}
+
+addButton.addEventListener('click', addBook);
