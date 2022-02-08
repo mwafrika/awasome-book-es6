b79d0b3df0b6f7826bf936b6e1417439771d9330
vikipretium <vighneshpasupathy@gmail.com>
2022-02-08 10:34:25 +0530

    add collection

---
+++ assets/js/book-manager.js
@@ -0,0 +1,8 @@
+const inputTitle = document.querySelector('.input-title');
+const inputAuthor = document.querySelector('.input-author');
+const addButton = document.querySelector('.add-button');
+const bookShelf = document.querySelector('.book-shelf');
+
+let bookList = [];
+
+
