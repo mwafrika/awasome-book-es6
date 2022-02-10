ff3404653b9a51556c1b0c64d1addf74bc587084
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-10 11:02:57 +0200

    Create book using class

--- assets/js/book-manager.js
+++ assets/js/book-manager.js
@@ -2,6 +2,7 @@ const inputTitle = document.querySelector('.input-title');
 const inputAuthor = document.querySelector('.input-author');
 const bookShelf = document.querySelector('.book-shelf');
 const bookCard = document.createElement('div');
+const bookForm = document.querySelector('.book-input');
 
 class Book {
   constructor(title, author) {
@@ -9,36 +10,30 @@ class Book {
     this.author = author;
   }
 
-  randomId() {
-    return Math.round(Math.random() * 10000000);
-  }
-
-  populateLocalStorage() {
-    if (!localStorage.getItem('books')) {
+  populateLocalStorage = () => {
+    let books = this.getBooks();
+    if (!books) {
       localStorage.setItem('books', JSON.stringify([]));
     }
-  }
-  clearFields() {
-    inputTitle.value = '';
-    inputAuthor.value = '';
-  }
+  };
 
-  getBooks() {
+  getBooks = () => {
     const books = JSON.parse(localStorage.getItem('books'));
-    return books;
-  }
-  removeBook(id) {
+    return books || [];
+  };
+
+  removeBook = (id) => {
     const books = this.getBooks();
     const newBooks = books.filter((book) => book.id.toString() !== id);
     localStorage.setItem('books', JSON.stringify(newBooks));
     document.querySelector(`#container${id}`).remove();
-  }
-  addBook(book) {
+  };
+  addBook = (book) => {
     const books = this.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
-  }
-  showBook() {
+  };
+  showBook = () => {
     this.populateLocalStorage();
     const books = this.getBooks();
     bookCard.innerHTML = '';
@@ -62,84 +57,20 @@ class Book {
       }
       bookShelf.appendChild(bookCard);
     }
-  }
-
+  };
   removeBookFromLocalStorage(id) {
     this.removeBook(id);
     this.showBook();
   }
 }
 
-// const randomId = () => Math.round(Math.random() * 100);
+const book = new Book(inputTitle.value, inputAuthor.value);
 
-// function populateLocalStorage() {
-//   if (!localStorage.getItem('books')) {
-//     localStorage.setItem('books', JSON.stringify([]));
-//   }
-// }
-
-// function clearFields() {
-//   inputTitle.value = '';
-//   inputAuthor.value = '';
-// }
-
-// function getBooks() {
-//   const books = JSON.parse(localStorage.getItem('books'));
-//   return books;
-// }
-
-// function removeBook(id) {
-//   const books = getBooks();
-//   const b = books.filter((book) => book.id.toString() !== id);
-//   localStorage.setItem('books', JSON.stringify(b));
-//   document.querySelector(`#container${id}`).remove();
-// }
-
-// function addBook(book) {
-//   const books = getBooks();
-//   books.push(book);
-//   localStorage.setItem('books', JSON.stringify(books));
-// }
-
-// function removeBookFromLocalStorage(id) {
-//   removeBook(id);
-//   showBook();
-// }
-
-// function showBook() {
-//   populateLocalStorage();
-//   const books = getBooks();
-//   bookCard.innerHTML = '';
-//   for (let i = 0; i < books.length; i += 1) {
-//     const book = books[i];
-//     bookCard.innerHTML += `
-//         <div id="container${book.id}">
-//         <p>${book.title}</p>
-//         <p>${book.author}</p>
-//         <button type="button" id="${book.id}" class="remove-button">Remove</button><br><br>
-//         <hr>
-//         </div>
-//         `;
-//     const removeButton = document.querySelectorAll('.remove-button');
-//     if (removeButton.length) {
-//       removeButton.forEach((button) => {
-//         button.addEventListener('click', () => {
-//           removeBookFromLocalStorage(button.id);
-//         });
-//       });
-//     }
-//     bookShelf.appendChild(bookCard);
-//   }
-// }
-
-// showBook();
-
-document.querySelector('.book-input').addEventListener('submit', (e) => {
+bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const book = new Book(inputTitle.value, inputAuthor.value);
-  const title = book.title;
-  const author = book.author;
-  const id = book.randomId();
+  const { title, author } = book;
+  const id = Math.round(Math.random() * 10000000);
 
   const bookObjt = {
     id: id,
@@ -148,7 +79,9 @@ document.querySelector('.book-input').addEventListener('submit', (e) => {
   };
   if (title && author) {
     book.addBook(bookObjt);
-    book.clearFields();
+    bookForm.reset();
     book.showBook();
   }
 });
+
+window.addEventListener('DOMContentLoaded', book.showBook());
